ebdd3a9723f63194833d47d6b424d521e244b1e6
Bosseskompis <bosseskompis@gmail.com>
2019-04-24 10:17:23 +0200

    Add react config

---
+++ react.js
@@ -0,0 +1,33 @@
+module.exports = {
+    env: {
+        browser: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        './index.js',
+    ],
+    plugins: [
+        'react',
+    ],
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+            legacyDecorators: true,
+            experimentalDecorators: true,
+            jsx: true,
+        },
+    },
+	rules: {
+        'no-console': 'off',
+        // Although it's nice to have, we don't want to force prop-type
+        // declaration as we haven't done it at all so far
+        'react/prop-types': 'off',
+    }
+}
